f1ea750e694ec87773bcd15aa58a61a69e598618
Indranil1998das <indranil199820das@gmail.com>
2025-06-14 17:57:48 +0530

    ChatApp

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,13 @@
 const ex = require("express");
 const app = ex();
-// const bodyParser = require("body-parser");
 const cookieParsar = require("cookie-parser");
 const userRoutes = require("./routes/userRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const Error = require("./middleware/Error");
 const ErrorThrow = require("./utils/ErrorThrow");
-// const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 // Middleware
 app.use(ex.json({ limit: "50mb" }));
-// app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cookieParsar());
 app.use(fileUpload());
 app.use("/api/v1", userRoutes, messageRoutes);
